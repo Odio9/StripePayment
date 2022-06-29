03921a38dd44132c7324eaba7ab76fb0960a4a81
Zach Odio <zachariaodio@gmail.com>
2022-06-29 12:27:04 -0600

    Upgrade react-native

---
+++ android/app/src/main/java/com/stripepayment/MainActivity.java
@@ -0,0 +1,25 @@
+package com.stripepayment;
+
+import com.facebook.react.ReactActivity;
+import android.content.res.Configuration;
+import android.content.Intent;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "StripePayment";
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Intent intent = new Intent("onConfigurationChanged");
+        intent.putExtra("newConfig", newConfig);
+        this.sendBroadcast(intent);
+    }
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import SplashScreen from './Source/View/SplashScreen/splashscreen.js';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => SplashScreen);
